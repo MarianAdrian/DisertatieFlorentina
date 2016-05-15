67da38febc1cf11726df01023e0d4ef920da4e01
MarianAdrian <marianadrian91@gmail.cpom>
2016-05-15 17:03:24 +0300

    added mergevec.py(not working yet)

---
+++ opencv-haar-classifier-training/mergevec/make_mergevec.sh
@@ -0,0 +1,4 @@
+g++.exe `pkg-config --libs --cflags opencv` -I. -o /c/Users/NiculaeAdrian/Documents/Git/DisertatieFlorentina/opencv3.1.0/opencv/sources/apps/haartraining/mergevec /c/Users/NiculaeAdrian/Documents/Git/DisertatieFlorentina/opencv3.1.0/opencv/sources/apps/haartraining/mergevec.cpp\
+  /c/Users/NiculaeAdrian/Documents/Git/DisertatieFlorentina/opencv3.1.0/opencv/sources/apps/haartraining/cvboost.cpp /c/Users/NiculaeAdrian/Documents/Git/DisertatieFlorentina/opencv3.1.0/opencv/sources/apps/haartraining/cvcommon.cpp /c/Users/NiculaeAdrian/Documents/Git/DisertatieFlorentina/opencv3.1.0/opencv/sources/apps/haartraining/cvsamples.cpp /c/Users/NiculaeAdrian/Documents/Git/DisertatieFlorentina/opencv3.1.0/opencv/sources/apps/haartraining/cvhaarclassifier.cpp\
+  /c/Users/NiculaeAdrian/Documents/Git/DisertatieFlorentina/opencv3.1.0/opencv/sources/apps/haartraining/cvhaartraining.cpp\
+  -lopencv_core -lopencv_calib3d -lopencv_imgproc -lopencv_highgui -lopencv_objdetect
---
+++ opencv-haar-classifier-training/mergevec/mergevec.bat
@@ -0,0 +1,1 @@
+python mergevec.py -v ..\samples -o merged.vec
