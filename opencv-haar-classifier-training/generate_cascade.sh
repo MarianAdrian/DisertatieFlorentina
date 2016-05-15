8149cf25c11ca20f5ebbd81fc11fece2b85558d9
MarianAdrian <marianadrian91@gmail.cpom>
2016-05-15 20:38:27 +0300

    added shell script for cascade training

---
+++ opencv-haar-classifier-training/generate_cascade.sh
@@ -0,0 +1,4 @@
+opencv_traincascade -data classifier -vec merged.vec -bg negatives.txt\
+  -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 1000\
+  -numNeg 600 -w 88 -h 40 -mode ALL -precalcValBufSize 1024\
+  -precalcIdxBufSize 1024
