252bd55afd3f17a895b2a117cedba403d0605565
MarianAdrian <marianadrian91@gmail.cpom>
2016-05-26 19:33:11 +0300

    insalled python packages(only opencv3 left to install)

---
+++ tools/Python3.4.4/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\NiculaeAdrian\Documents\Git\DisertatieFlorentina\tools\Python3.4.4\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
